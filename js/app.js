4c1011c42f8a821c4edbd5add859a730e4260eb4
ana_nenadic <ana.nenadic18@gmail.com>
2018-02-09 21:07:04 +0100

    reload and open cards functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
  * Create a list that holds all of your cards
  */
 
+let cards = $(".card");
+/*let cards = [...card];*/
+
+
+
 
 /*
  * Display the cards on the page
@@ -25,7 +30,19 @@ function shuffle(array) {
     return array;
 }
 
+shuffle(cards);
 
+let newCardsHtml = "";
+
+$('.restart').click(function(){
+	location.reload();
+});
+
+for (const item of cards){
+	newCardsHtml += $(item).prop('outerHTML');
+}
+
+$('.deck').html(newCardsHtml);
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +53,28 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+let openCards = [];
+
+$(".card").click(function(){
+    $(this).addClass("open show");
+    openCards.push($(this));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
