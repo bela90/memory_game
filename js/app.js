e45cfcb9f4851bdd651c7ccf25679ca933c58c7f
ana_nenadic <ana.nenadic18@gmail.com>
2018-02-12 22:35:08 +0100

    first finished solid version

--- js/app.js
+++ js/app.js
@@ -1,21 +1,116 @@
-/*
- * Create a list that holds all of your cards
- */
+
+
+let openCards = [];
+let clickCounter = 0;
+let timerStarted = false; 
+let timer;
+let seconds = 0;
+let minutes = 0;
+let starsNumber = 3;
+let newCardsHtml = "";
 
 let cards = $(".card");
-/*let cards = [...card];*/
 
 
+// on page reload shuffle functions is called and shuffles array of cards
+shuffle(cards);
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// builting HTML string that is representing new deck after shuffling
+for (const item of cards) {
+    newCardsHtml += $(item).prop('outerHTML');
+}
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+// inserting HTML of the cards into deck div
+$('.deck').html(newCardsHtml);
+
+
+// adding click functionality 
+$(".card").click(function() {
+
+    const currentCard = $(this);
+
+    clickCounter++;
+
+    // starting timer on first click 
+    if (timerStarted === false){
+      startTimer();
+      timerStarted = true;
+    }
+
+    //update number of moves
+    $('.moves').html(clickCounter);
+
+    starRating();
+    
+    //if player clicks on already opened card, ignore it  
+    if (currentCard.hasClass("matched")){
+        return;
+    }
+
+    // displaying card's symbols
+    currentCard.addClass("open show");
+    
+    
+    openCards.push(currentCard);
+
+
+    // on every second click compare current card with previous 
+    if ((openCards.length % 2) === 0) {
+
+        const previousCard = $(openCards[openCards.length - 2]);
+
+        //if player clicks on same cards, close them and remove from openCards 
+        if (previousCard.is(currentCard)) {
+            
+            currentCard.toggleClass("open show");
+
+            openCards.pop();
+            openCards.pop();
+        }
+        //if cards matched
+        else if ((previousCard.prop('innerHTML')) == (currentCard.prop('innerHTML'))) {
+
+            currentCard.addClass("matched");
+            previousCard.addClass("matched");
+            
+            if (openCards.length === 16){
+
+                clearInterval(timer);
+
+                setTimeout(function(){
+
+                    let result = confirm ("Congratulations!\n Your time is: " + minutes + ":" + seconds +
+                                          "\n Your score is: " + starsNumber + " stars. \n Play again?");
+                        
+                        if (result === true){
+                            location.reload();
+                        }
+
+                },300);
+            }
+        // if cards are different, close them 
+        }else{
+            setTimeout(function(){
+                currentCard.toggleClass("open show");
+                previousCard.toggleClass("open show");
+
+                openCards.pop();
+                openCards.pop();
+                
+            },400);
+        }
+    }
+});
+
+
+// reload function
+$('.restart').click(function() {
+    location.reload();
+});
+
+
+// Shuffle function 
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
@@ -31,141 +126,45 @@ function shuffle(array) {
     return array;
 }
 
-shuffle(cards);
 
-let newCardsHtml = "";
-
-$('.restart').click(function() {
-    location.reload();
-});
-
-for (const item of cards) {
-    newCardsHtml += $(item).prop('outerHTML');
-}
-
-$('.deck').html(newCardsHtml);
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-let openCards = [];
-
-let clickCounter = 0;
-
-let timerStarted = false; 
-
-let timer;
-
-
- 
-
-$(".card").click(function() {
-
-    const currentCard = $(this);
-
-    if (timerStarted === false){
-      startTimer();
-      timerStarted = true;
-    }
-    
-    
-
-    currentCard.addClass("open show");
-    openCards.push(currentCard);
-
-    console.log(currentCard);
-
-    clickCounter++;
-
-    starRating();
-
-    $('.moves').html(clickCounter);
-
-
-
-
-    if ((clickCounter % 2) === 0) {
-
-        const previousCard = $(openCards[openCards.length - 2]);
-
-        console.log(previousCard);
-        console.log(currentCard);
-
-        if (previousCard.is(currentCard)) {
-            
-            currentCard.toggleClass("open show");
-
-            openCards.pop();
-            openCards.pop();
-        }
-
-        else if ((previousCard.prop('innerHTML')) == (currentCard.prop('innerHTML'))) {
-            
-            if (openCards.length === 16){
-                clearInterval(timer);
-
-                setTimeout(function(){
-
-                alert ("Congratulations!");
-            },300);
-            }
-
-        } else{
-           
-            setTimeout(function(){
-                currentCard.toggleClass("open show");
-                previousCard.toggleClass("open show");
-
-                openCards.pop();
-                openCards.pop();
-            },400);
-        }
-
-        console.log("Kliknuto je " + clickCounter);
-    }
-
-
-});
-
-
+// Timer 
 function startTimer(){
 
-        var sec = 0;
-        
-        function pad ( val ) { 
+  timer = setInterval( function(){
 
-            return val > 9 ? val : "0" + val; 
-        }
-
-        timer = setInterval( function(){
-            $("#seconds").html(pad(++sec%60));
-            $("#minutes").html(pad(parseInt(sec/60,10)));
+    seconds++;
     
-        }, 1000);
+        if(seconds == 60){
+        
+            minutes++;
+            
+            seconds = 0;
+        }
+        
+    $("#seconds").html(seconds);
+    $("#minutes").html(minutes);
 
+    }, 1000);
 }
 
 
+// Star Rating function that depending on number of moves, updates score of users
 function starRating(){
-    if(clickCounter === 5){
+
+    if(clickCounter === 40){
         $('.stars > li:nth-child(3)').html('<i class="fa fa-star-o"></i>');
+        --starsNumber;
     }
 
-    if(clickCounter === 10){
+    if(clickCounter === 45){
         $('.stars > li:nth-child(2)').html('<i class="fa fa-star-o"></i>');
+        --starsNumber;
     }
 
-    if(clickCounter === 15){
+    if(clickCounter === 50){
         $('.stars > li:nth-child(1)').html('<i class="fa fa-star-o"></i>');
+        --starsNumber;
     }
-
 }
 
 
